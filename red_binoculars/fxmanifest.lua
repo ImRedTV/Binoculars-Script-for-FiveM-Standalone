1a7305ae52fe2a072f68d6236367cd4fbee14dbe
RED <50785550+ImRedTV@users.noreply.github.com>
2024-09-11 17:45:34 +0200

    Create fxmanifest.lua

---
+++ red_binoculars/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Binoculars Script for FiveM (Standalone)
+-- Created by RED
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'RED'
+description 'Standalone Binoculars Script for FiveM'
+version '1.0.0'
+
+client_script 'binoculars.lua'
